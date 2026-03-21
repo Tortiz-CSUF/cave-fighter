3c7bdbde601ac8a975c4d342915b85a8faa7ebcf
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-21 02:37:14 +0000

    cave-fighter_Interim_4

--- scripts/viking.gd
+++ scripts/viking.gd
@@ -25,21 +25,43 @@ func _ready() -> void:
 	anim.animation_finished.conect(_on_animation_finished)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	if is_dead:
+		return
+		
+	if is_hurt: 
+		velocity.y += GRAVITY * delta
+		move_and_slide()
+		return
+	if not game_started:
+		velocity.y += GRAVITY * delta
+		velocity.x = 0
+		move_and_slide()
+		_play_anim("idle")
+		return
+		
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+		velocity.y += GRAVITY * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		can_double_jump = true
+		
+	if is_attacking:
+		if not is_on_floor():
+			velocity.y += GRAVITY * delta
+		velocity.x = 0
+		move_and_slide()
+		return
+		
+	if Input.is_action_just_pressed("viking_jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = true
+		elif can_double_jump:
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = false
+			
+	
+		
+		
 
 	move_and_slide()
 
@@ -105,14 +127,6 @@ func take_damage(amount: float, from_right: bool) -> void:
 		is_attacking = false
 		anim.flip.h = not facing_right
 		_play_anim("hurt")	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
